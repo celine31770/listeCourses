737f67c12c0c6f33867018d2d99ff8c0c230c85c
Laurène <Laurne@192.168.1.69>
2018-09-03 16:54:02 +0200

    Update UserDao

--- Liste_courses/src/com/listecourses/dao/User_Dao.java
+++ Liste_courses/src/com/listecourses/dao/User_Dao.java
@@ -1,11 +1,28 @@
 package com.listecourses.dao;
 
+import java.util.ArrayList;
+
+import com.listecourses.models.*;
 import com.listecourses.models.User_;
 
 public class User_Dao implements IAbstractDao<User_> {
 	
+	// On créé la liste de user en appelant la méthode generateUserList
+	public ArrayList<User_> userList = PopulateUser_.generateUserList();
 	
-
+	
+	
+	public static void Init() {
+		ArrayList<Contact> listContact = PopulateContact.generateContactList();
+		
+		// On va boucler sur tous les contacts dans listContact
+		for(Contact c:listContact) {
+			// On va comparer les users
+			//userList.
+			//c.getUserId()
+		}
+	}
+	
 	@Override
 	public void create(User_ entity) {
 		// TODO Auto-generated method stub
