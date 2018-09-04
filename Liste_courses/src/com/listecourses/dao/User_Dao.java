e92503ca1bf87e66d71ad9d08a580bd82f9404c9
celin <celine.mercadier@devwebmobiles.fr>
2018-09-04 11:32:56 +0200

    ajout des contacts a la liste contact

--- Liste_courses/src/com/listecourses/dao/User_Dao.java
+++ Liste_courses/src/com/listecourses/dao/User_Dao.java
@@ -8,19 +8,24 @@ import com.listecourses.models.User_;
 public class User_Dao implements IAbstractDao<User_> {
 	
 	// On créé la liste de user en appelant la méthode generateUserList
-	public ArrayList<User_> userList = PopulateUser_.generateUserList();
+	public static  ArrayList<User_> userList = PopulateUser_.generateUserList();
 	
 	
 	
 	public static void Init() {
 		ArrayList<Contact> listContact = PopulateContact.generateContactList();
 		
-		// On va boucler sur tous les contacts dans listContact
-		for(Contact c:listContact) {
-			// On va comparer les users
-			//userList.
-			//c.getUserId()
+		// On va boucler sur tous les users dans userList
+		for(User_ u: userList){ // pour chaque user de la liste
+			for(Contact c:listContact){  // on parcours tous les contacts
+				//s'il y a correspondance entre le user de la userList et le userId dans la listContact
+				if(u.equals(c.getUserId())){// On va comparer les users
+					//on ajoute le contact à la liste du user en cours
+					u.addContact(c);
+				}
+			}
 		}
+
 	}
 	
 	@Override
